f624f18140b1d350e66da738e186cbe34976f93d
Fl0wwDev <mafradin@hotmail.fr>
2024-10-25 13:38:13 +0200

    ajout de package

---
+++ MAKAROV-AIRPORT/docker/djangopache-compose/makarov_gp/static/libs/assets/js/faro-initialization.js
@@ -0,0 +1,15 @@
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+    url: 'https://faro-collector-prod-us-west-0.grafana.net/collect/07b39176d47f619861f0773f090353a2',
+    app: {
+        name: 'django',
+        version: '1.0.0',
+        environment: 'production'
+    },
+    instrumentations: [
+        ...getWebInstrumentations(),
+        new TracingInstrumentation(),
+    ],
+});
