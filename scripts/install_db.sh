c33fda88de270dee2f3d5c82d7b734b4c93a1983
Gabriel <gabrielgarrone670@gmail.com>
2024-12-06 11:39:09 +0100

    Refactor Kubernetes deployment and persistent volume claim configurations; update labels and remove unnecessary annotations

--- scripts/install_db.sh
+++ scripts/install_db.sh
@@ -1,7 +1,7 @@
 sudo apt update
-sudo apt install mysql-server -y
-sudo apt install mysql-server -y
-sudo systemctl start mysql
+sudo apt install mariadb-server -y
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
 sudo mysql -e "CREATE DATABASE makarov_airport;"
 sed -i 's/utf8mb4_uca1400_ai_ci/utf8mb4_general_ci/g' /vagrant/MAKAROV-AIRPORT/makarov_airport_dump.sql
 sudo mysql -u root makarov_airport < /vagrant/MAKAROV-AIRPORT/makarov_airport_dump.sql
