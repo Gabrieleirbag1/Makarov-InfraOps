f743dc802a359519c2c20cb59e6b31f1270e5ca5
Mahé FRADIN <101654547+Fl0wwDev@users.noreply.github.com>
2024-12-10 11:25:57 +0100

    Update apply_all.sh

--- scripts/apply_all.sh
+++ scripts/apply_all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /vagrant/MAKAROV-AIRPORT/docker
 COMPOSE_DIR="compose"
-GRAFANA_DIR="grafana-kubernetes"
 
 if [ ! -d "$COMPOSE_DIR" ]; then
   echo "Directory $COMPOSE_DIR does not exist."
@@ -19,25 +18,3 @@ for file in "$COMPOSE_DIR"/*.yaml; do
 done
 
 echo "All files in $COMPOSE_DIR applied."
-
-if [ ! -d "$GRAFANA_DIR" ]; then
-  echo "Directory $GRAFANA_DIR does not exist."
-  exit 1
-fi
-
-echo "Installing Grafana using Helm..."
-helm upgrade --install grafana prometheus-community/grafana --namespace monitoring --create-namespace -f "$GRAFANA_DIR/values.yaml"
-
-echo "Grafana deployed successfully."
-
-echo "Forwarding port 3000 to Grafana service..."
-kubectl port-forward svc/grafana 3000:80 -n monitoring &
-
-sleep 5
-
-echo "Retrieving Grafana password..."
-GRAFANA_PASSWORD=$(kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode)
-
-echo "Grafana password: $GRAFANA_PASSWORD"
-
-echo "You can now access Grafana at http://localhost:3000 with the password above."
